684fadeb003b14d93c05237f5fcbcc49c2851651
Marco <marrco.gonzalez@gmail.com>
2024-11-10 01:02:46 -0300

    Vercel5

--- mi_proyecto/settings.py
+++ mi_proyecto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY_env', default='tu_clave_secreta_aqui')
+SECRET_KEY = config('SECRET_KEY_env')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
