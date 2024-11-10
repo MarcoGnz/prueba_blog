f7bca46be551713e2658a3c248a5a2bff0bd8a7f
Marco <marrco.gonzalez@gmail.com>
2024-11-09 22:46:46 -0300

    first commit

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from .models import Post
+
+def lista_posts(request):
+  
+  posts = Post.objects.all()
+  
+  return render(request, 'blog/lista_posts.html', {'posts': posts}) 
+
+# Create your views here.
